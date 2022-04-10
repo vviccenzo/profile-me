78dc631c8e568c4b87d04c3ad7bacea564845ad0
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-10 15:09:02 -0300

    Atualizando

---
+++ game/script.js
@@ -0,0 +1,37 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+    if(dino.classList != "jump") {
+        dino.classList.add("jump");
+
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 300);
+}};
+
+let isAlive = setInterval(function () {
+
+        // pegar posição atual do dino no Y
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    
+        // pegar posição atual do cactus no Y//
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    console.log(cactusLeft)
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert("Você bateu no cacto.\nVocê será redirecionado para a página inicial.");
+        cactusLeft = 0;
+        mudarPagina()
+    }
+
+
+}, 10)
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
+
+function mudarPagina(){
+    window.location = "bootcamp-profile-euax/game/index.html";
+}
