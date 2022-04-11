32035b548c6eea021d0d48faf9311813fdf04878
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-11 07:19:34 -0300

    main

---
+++ forms/script.js
@@ -0,0 +1,8 @@
+function teste() {
+    
+    const name = document.getElementById('name').value
+    const mail = document.getElementById('mail').value
+    const area = document.getElementById('area').value
+    
+    return alert(`Assim que possível, ${name} o Desenvolvedor entrará em contato.`)
+}
